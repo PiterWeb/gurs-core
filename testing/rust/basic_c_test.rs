d1b0bcc5f1ee574bc04864abbfa67d1b67fc48bb
PiterDev <piterzdev@gmail.com>
2024-01-24 23:43:40 +0100

    testing - rust file renamed

--- testing/rust/basic_c_test.rs
+++ testing/rust/basic_c_test.rs
@@ -8,7 +8,6 @@ pub extern "C" fn twoParams(xd: str, lol: i32) {
     println!(":?", xd);
 }
 
-
 #[no_mangle]
 pub unsafe extern "C" fn unsafeReturn() -> &str {
     "Hello World";
--- testing/testing_config_test.go
+++ testing/testing_config_test.go
@@ -12,9 +12,9 @@ const (
 	GO_COMPATIBLE_FUNCTIONS = 3
 )
 
-const MOCK_RUST_FN = `[{"fileName":"rust\\basic_test","name":"paramVec","parameters":[{"name":"adios","valueType":"Vec\u003c\u0026str\u003e"}],"returnType":""},{"fileName":"rust\\basic_test","name":"twoParams","parameters":[{"name":"xd","valueType":"str"},{"name":"lol","valueType":"i32"}],"returnType":""},{"fileName":"rust\\basic_test","name":"unsafeReturn","parameters":[],"returnType":"\u0026str"}]`
+const MOCK_RUST_FN = `[{"fileName":"rust\\basic_c_test","name":"paramVec","parameters":[{"name":"adios","valueType":"Vec\u003c\u0026str\u003e"}],"returnType":""},{"fileName":"rust\\basic_c_test","name":"twoParams","parameters":[{"name":"xd","valueType":"str"},{"name":"lol","valueType":"i32"}],"returnType":""},{"fileName":"rust\\basic_c_test","name":"unsafeReturn","parameters":[],"returnType":"\u0026str"}]`
 
-const MOCK_GO_FN = `[{"fileName":"rust\\basic_test","name":"ParamVec","parameters":[{"name":"adios","valueType":"[]string"}],"returnType":""},{"fileName":"rust\\basic_test","name":"TwoParams","parameters":[{"name":"xd","valueType":"string"},{"name":"lol","valueType":"int32"}],"returnType":""},{"fileName":"rust\\basic_test","name":"UnsafeReturn","parameters":[],"returnType":"string"}]`
+const MOCK_GO_FN = `[{"fileName":"rust\\basic_c_test","name":"ParamVec","parameters":[{"name":"adios","valueType":"[]string"}],"returnType":""},{"fileName":"rust\\basic_c_test","name":"TwoParams","parameters":[{"name":"xd","valueType":"string"},{"name":"lol","valueType":"int32"}],"returnType":""},{"fileName":"rust\\basic_c_test","name":"UnsafeReturn","parameters":[],"returnType":"string"}]`
 
 func getMockRustFunctions() []parser.Rustfn {
 
